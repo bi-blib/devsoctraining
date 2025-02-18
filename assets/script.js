31188ad564871ea66bed7fa7e8b36e0b50eb9c52
Bianca Mori Zegarra <z5534123@ad.unsw.edu.au>
2025-02-18 20:37:30 +1100

    javascript test, fixed function declaration

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,7 @@
+alert("Script is working!");
 function toggle(id) {
     const content = document.getElementById(`content-${id}`);
-    content.style.display = content.style.display === "none" ? "block" : "none";
+    if (content) {
+        content.classList.toggle("active");
+    }
 }
